0165d08b8d5bce4648dfb28f4c8d29e785abec45
Vincent Schlosser <s.vincent@live.fr>
2020-04-14 16:32:29 +0200

    Change role format

--- models/game.js
+++ models/game.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 const gameSchema = new mongoose.Schema({
     id: Number,
     board: [{
         word: String,
         color: String,
-        discovered: Boolean,
+        discovered: {type: Boolean, default: false},
     }],
     players: [{
         socketId: String,
         pseudo: String,
         team: { type: String, enum: ['blue', 'red'] },
-        role: { type: String, enum: ['spy', 'agent'] },
-    }]
-})
+        role: { type: String, enum: ['BS', 'BA', 'RS', 'RA'] },
+    }],
+    turn: { type: String, enum: ['BS', 'BA', 'N', 'RS', 'RA'], default: 'BS' }
+});
 
-module.exports = mongoose.model('Game', gameSchema )
+module.exports = mongoose.model('Game', gameSchema );
